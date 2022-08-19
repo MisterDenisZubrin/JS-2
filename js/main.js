264b5aaef8af1e5bdbd854474b9eda95d3da65fe
Denis <dendel1998@gmail.com>
2022-08-19 13:40:41 +0300

    Задание 2

--- js/main.js
+++ js/main.js
@@ -1,40 +1,114 @@
-const goods = [
-  {
-    title: 'Shirt',
-    price: 150,
-    img: '../img/shirt.jpg'
-  },
-  {
-    title: 'Socks',
-    price: 50,
-    img: '../img/socks.jpg'
-  },
-  {
-    title: 'Jacket',
-    price: 350,
-    img: '../img/jacket.jpg'
-  },
-  {
-    title: 'Shoes',
-    price: 250,
-    img: '../img/shoes.jpg'
-  },
-  {
-    
-  },
-];
-const renderGoodsItem = (img = '../img/placeholder.jpg',title = 'Product', price = 0) => {
-  return `
-    <div class="goods-item">
-      <img class="goods-item-img" src="${img}" alt="product picture">
-      <h3 class="goods-item-title">${title}</h3>
-      <p class="goods-item-price">${price}</p>
+'use strict';
+
+class ProductsList {
+  constructor(container = '.goods-list') {
+    this.container = container;
+    this.goods = [];
+    this._fetchGoods();
+    this._render();
+  }
+
+  _fetchGoods() {
+    this.goods = [{
+        id: 1,
+        title: 'Shirt',
+        price: 150,
+        img: '../img/shirt.jpg'
+      },
+      {
+        id: 2,
+        title: 'Socks',
+        price: 50,
+        img: '../img/socks.jpg'
+      },
+      {
+        id: 3,
+        title: 'Jacket',
+        price: 350,
+        img: '../img/jacket.jpg'
+      },
+      {
+        id: 4,
+        title: 'Shoes',
+        price: 250,
+        img: '../img/shoes.jpg'
+      },
+      {
+
+      },
+    ];
+  }
+
+  #countTotalPrice() {
+    let totalPrice = 0;
+    for (let product of this.goods) {
+      if (product.price) {
+        totalPrice += product.price;
+      }
+    }
+    return totalPrice;
+  }
+
+  _render() {
+    const container = document.querySelector(this.container);
+    for (let item of this.goods) {
+      const product = new Product(item);
+      container.insertAdjacentHTML('beforeend', product._render());
+    }
+    container.insertAdjacentHTML('afterend', `Общая стоимость товаров равна: ${this.#countTotalPrice()}`);
+  }
+}
+
+class Product {
+  constructor(product) {
+    this.title = product.title || 'Product';
+    this.id = product.id;
+    this.price = product.price || 0;
+    this.img = product.img || '../img/placeholder.jpg';
+  }
+
+  _render() {
+    return `
+    <div class="goods-item" id="${this.id}">
+      <img class="goods-item-img" src="${this.img}" alt="product picture">
+      <h3 class="goods-item-title">${this.title}</h3>
+      <p class="goods-item-price">${this.price}</p>
       <button class="goods-item-button button">Добавить</button>
     </div>
   `;
-};
-const renderGoodsList = (list) => {
-  const goodsList = list.map(item => renderGoodsItem(item.img, item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+  }
 }
-renderGoodsList(goods);
+
+const productsList = new ProductsList();
+
+class Basket {
+  constructor(container = '.basket') {
+    this.container = container;
+  }
+
+  _render() {}
+
+  #countTotalPrice() {
+
+  }
+}
+
+class BasketItem {
+  constructor(product) {
+
+  }
+
+  _removeFromBasket() {
+
+  }
+
+  _increaseAmount() {
+
+  }
+
+  _decreaseAmount() {
+
+  }
+
+  _render() {}
+}
